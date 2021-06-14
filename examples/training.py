0e72d44472e3034d33e3fdcd0b80a774c6f67ce7
Yannick Uhlmann <augustunderground@protonmail.com>
2021-06-14 14:39:36 +0200

    quotes

--- examples/training.py
+++ examples/training.py
@@ -1,15 +1,16 @@
+import yaml
 import torch
 from pytorch_lightning import Trainer
 from precept import PreceptModule, PreceptDataModule
 
 # File System setup
-device_name = "ptmn90"
-data_path = "/tmp/precept/data/90nm_bulk_nmos.h5"
-model_path = "/tmp/precept/example"
+device_name = 'ptmn90'
+data_path = '/tmp/precept/data/90nm_bulk_nmos.h5'
+model_path = '/tmp/precept/example'
 
 # Specify which columns in the data base are inputs and outputs
-params_x = [ "W", "L", "Vgs", "Vds", "Vbs" ]
-params_y = [ "vdsat", "id", "gm", "gds", "fug" ]
+params_x = [ 'W', 'L', 'Vgs', 'Vds', 'Vbs' ]
+params_y = [ 'vdsat', 'id', 'gm', 'gds', 'fug' ]
 
 # Infer number if input and output neurons
 num_x = len(params_x)
@@ -17,7 +18,7 @@ num_y = len(params_y)
 
 # Specify which parameters need to be transformed
 trafo_mask_x = []
-trafo_mask_y = [ "id", "gm", "gds", "fug" ]
+trafo_mask_y = [ 'id', 'gm', 'gds', 'fug' ]
 
 # Specify the transformation paramter
 lambdas_x = []
@@ -58,11 +59,35 @@ trainer = Trainer( gpus = 1                             # Number of available GP
 trainer.fit(model, data)
 
 # Save a checkpoint of the current model
-trainer.save_checkpoint(f"{model_path}/example.ckpt")
+trainer.save_checkpoint(f'{model_path}/example.ckpt')
+
+# Save Metadata for Inference
+meta_file = f'{model_path}/example.yml'
+
+meta_data = { 'num_x':      num_x
+            , 'num_y':      num_y
+            , 'params_x':   params_x
+            , 'params_y':   params_y
+            , 'mask_x':     trafo_mask_x
+            , 'mask_y':     trafo_mask_y
+            , 'min_x':      data.min_x.tolist()
+            , 'max_x':      data.max_x.tolist()
+            , 'min_y':      data.min_y.tolist()
+            , 'max_y':      data.max_y.tolist()
+            , 'lambdas_x':  data.lambdas_x
+            , 'lambdas_y':  data.lambdas_y
+            , }
+
+yaml.Dumper.ignore_aliases = lambda *args: True
+with open(meta_file, 'w+') as yml:
+    yaml.dump( meta_data, yml
+             , allow_unicode = True
+             , default_flow_style = False
+             , )
 
 # Compile current model to TorchScript
-script = model.to_torchscript( method = "trace"
+script = model.to_torchscript( method = 'trace'
                              , example_inputs = torch.rand(1, num_x, dtype=float)
                              , )
 
-torch.jit.save(script, f"{model_path}/example.ckpt")
+torch.jit.save(script, f'{model_path}/example.pt')
